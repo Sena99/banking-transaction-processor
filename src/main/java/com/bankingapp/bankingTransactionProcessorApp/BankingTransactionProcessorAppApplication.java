c88a9e69b148326d8dccde2eced0a54d97f0f87f
senajava13-dot <sena.java13@gmail.com>
2026-09-18 14:13:34 +0530

    updated the project setup

---
+++ src/main/java/com/bankingapp/bankingTransactionProcessorApp/BankingTransactionProcessorAppApplication.java
@@ -0,0 +1,13 @@
+package com.bankingapp.bankingTransactionProcessorApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingTransactionProcessorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingTransactionProcessorAppApplication.class, args);
+	}
+
+}
